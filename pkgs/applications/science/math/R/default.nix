00e632c73aca924caa9bd626dd9342cfabdebfd6
Bruno Rodrigues <bruno@brodrigues.co>
2024-12-16 23:07:00 +0100

    fix R build see https://github.com/NixOS/nixpkgs/commit/7a5f0ec2d11cd715acb05d4634e8d28c17811c75#diff-ac5610da9de5de08bc82807139f47182d0a6320414ef42f95c2f7f9091c971c6R45

--- pkgs/applications/science/math/R/default.nix
+++ pkgs/applications/science/math/R/default.nix
@@ -32,8 +32,13 @@ stdenv.mkDerivation rec {
     ./fix-failing-test.patch
   ];
 
-  prePatch = stdenv.lib.optionalString stdenv.isDarwin ''
-    substituteInPlace configure --replace "-install_name libR.dylib" "-install_name $out/lib/R/lib/libR.dylib"
+  prePatch = lib.optionalString stdenv.isDarwin ''
+    substituteInPlace configure \
+      --replace "-install_name libRblas.dylib" "-install_name $out/lib/R/lib/libRblas.dylib" \
+      --replace "-install_name libRlapack.dylib" "-install_name $out/lib/R/lib/libRlapack.dylib" \
+      --replace "-install_name libR.dylib" "-install_name $out/lib/R/lib/libR.dylib"
+    substituteInPlace tests/Examples/Makefile.in \
+      --replace "test-Examples: test-Examples-Base" "test-Examples:" # do not test the examples
   '';
 
   dontDisableStatic = static;
