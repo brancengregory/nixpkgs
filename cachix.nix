fc597c49cc59d68b84ed4ca49134236946e92991
Bruno Rodrigues <brodriguesco@protonmail.com>
2025-02-28 12:09:49 +0100

    Update cachix.nix

--- cachix.nix
+++ cachix.nix
@@ -1,14 +1,11 @@
 let
  pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/refs/heads/r-daily.tar.gz") {};
  system_packages = builtins.attrValues {
-  inherit (pkgs) R quarto glibcLocalesUtf8 nix;
+  inherit (pkgs) R rstudio quarto glibcLocalesUtf8 nix;
 };
  r_packages = builtins.attrValues {
   inherit (pkgs.rPackages) tidyverse networkscaleup mlpack multinma cbq rts2 OpenMx glmmrBase seqtrie pema Boom psBayesborrow duckdb VariantAnnotation FlexReg s2 qeML rJava data_table readxl fixest collapse rstanarm terra sf stars vapour quarto Rcpp gdalcubes geos devtools fledge fusen codetools jsonlite httr sys testthat knitr stringi blavaan gpboost igraph rmarkdown;
 };
-  tex = (pkgs.texlive.combine {
-  inherit (pkgs.texlive) scheme-small;
-});
   in
   pkgs.mkShell {
     LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then  "${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive" else "";
