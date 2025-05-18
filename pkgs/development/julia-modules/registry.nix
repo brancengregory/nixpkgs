8adac0bd319fa48270c757309ed059fcb8e33f6f
Bruno Rodrigues <bruno@brodrigues.co>
2025-05-18 15:06:09 +0200

    correct hash for julia registry

--- pkgs/development/julia-modules/registry.nix
+++ pkgs/development/julia-modules/registry.nix
@@ -4,6 +4,6 @@ fetchFromGitHub {
   owner = "CodeDownIO";
   repo = "General";
   rev = "a357ab259a17ea8252ac8bbf4c7320b61c0d2b1d";
-  sha256 = "sha256-02wrdj9lv0avwr3vxvp8yaj82wrkcss5mxq8zm917xa87dcfbrsd";
+  sha256 = "sha256-TeflWDtI9RNS/Qj3WrRmM3OBpPLo7r5H5luBTZNsmQs=";
   # date = "2024-07-01T12:22:35+00:00";
 }
