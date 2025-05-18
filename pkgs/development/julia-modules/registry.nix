ad6d67484c8dffa4a9a08ee166e9dc44a336f880
Bruno Rodrigues <bruno@brodrigues.co>
2025-05-18 12:57:37 +0200

    Added Julia Registry at 2025-05-16

--- pkgs/development/julia-modules/registry.nix
+++ pkgs/development/julia-modules/registry.nix
@@ -3,7 +3,7 @@
 fetchFromGitHub {
   owner = "CodeDownIO";
   repo = "General";
-  rev = "998c6da1553dc0776dfff314d2f1bd5af488ed71";
-  sha256 = "sha256-57RiIPTu9895mdk3oSfo7I3PYw7G0BfJG1u+mYkJeLk=";
+  rev = "a357ab259a17ea8252ac8bbf4c7320b61c0d2b1d";
+  sha256 = "sha256-02wrdj9lv0avwr3vxvp8yaj82wrkcss5mxq8zm917xa87dcfbrsd";
   # date = "2024-07-01T12:22:35+00:00";
 }
