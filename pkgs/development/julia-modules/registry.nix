1c405f2f075b004a2e5e5f0c37e66afd8accd8ad
Bruno Rodrigues <bruno@brodrigues.co>
2025-07-28 12:27:05 +0000

    daily update

--- pkgs/development/julia-modules/registry.nix
+++ pkgs/development/julia-modules/registry.nix
@@ -3,7 +3,7 @@
 fetchFromGitHub {
   owner = "CodeDownIO";
   repo = "General";
-  rev = "998c6da1553dc0776dfff314d2f1bd5af488ed71";
-  sha256 = "sha256-57RiIPTu9895mdk3oSfo7I3PYw7G0BfJG1u+mYkJeLk=";
+  rev = "6e5a8c29895a3ee3c1f2a959de77fdc383efce9e";
+  sha256 = "19smk2nn8s6czpni25x5l9k00ncac5qk46fp3jqkiaksjpjajwlx";
   # date = "2024-07-01T12:22:35+00:00";
 }
