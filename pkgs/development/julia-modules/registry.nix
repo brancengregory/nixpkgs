b327cd31b01155607bc1ef47fc866f488edba963
Bruno Rodrigues <bruno@brodrigues.co>
2025-11-28 12:39:27 +0000

    daily update

--- pkgs/development/julia-modules/registry.nix
+++ pkgs/development/julia-modules/registry.nix
@@ -3,7 +3,7 @@
 fetchFromGitHub {
   owner = "CodeDownIO";
   repo = "General";
-  rev = "4b19a1dc55d2877e85a5d0e98702b75872210e9d";
-  sha256 = "sha256-mVeBTpEQnW3fvJu1+4T8z+earMjEgtdy0tnZnAxz/pk=";
+  rev = "73cc99a8e5751cedd9fa83ed9b3a0b5f955ac639";
+  sha256 = "0abmyl3qnflcbf2l258sk3g1c0mf0qa59nmhrlc7s4ba21g71pd4";
   # date = "2025-08-12T05:20:40+00:00";
 }
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16596,6 +16596,7 @@ self: super: with self; {
   rxv = callPackage ../development/python-modules/rxv { };
 
   ryd-client = callPackage ../development/python-modules/ryd-client { };
+  ryxpress = callPackage ../development/python-modules/ryxpress { };
 
   rzpipe = callPackage ../development/python-modules/rzpipe { };
 
@@ -18335,6 +18336,7 @@ self: super: with self; {
   tailscale = callPackage ../development/python-modules/tailscale { };
 
   takethetime = callPackage ../development/python-modules/takethetime { };
+  talvez = callPackage ../development/python-modules/talvez { };
 
   tami4edgeapi = callPackage ../development/python-modules/tami4edgeapi { };
 
