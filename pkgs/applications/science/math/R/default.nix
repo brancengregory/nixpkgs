7f3eec112f2fd7a47f4c082e8a616b6b3406d582
Bruno Rodrigues <brodriguesco@protonmail.com>
2026-02-17 20:16:18 +0100

    Add rpath for gfortran library in LDFLAGS

--- pkgs/applications/science/math/R/default.nix
+++ pkgs/applications/science/math/R/default.nix
@@ -82,7 +82,7 @@ stdenv.mkDerivation (finalAttrs: {
       --without-x
       OBJC="clang"
       CPPFLAGS="-isystem ${lib.getDev libcxx}/include/c++/v1"
-      LDFLAGS="-L${lib.getLib stdenv.cc.libcxx}/lib -L${lib.getLib gfortran}/lib"
+      LDFLAGS="-L${lib.getLib stdenv.cc.libcxx}/lib -L${lib.getLib gfortran}/lib -Wl,-rpath,${lib.getLib gfortran}/lib"
   '' + ''
     )
     echo >>etc/Renviron.in "TCLLIBPATH=${tk}/lib"
